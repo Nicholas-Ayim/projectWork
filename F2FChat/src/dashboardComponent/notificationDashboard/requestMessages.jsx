226e8898f4d1fc7fdd72d4d47df650a061c46171
Nicholas-Ayim <nicholasayim7@gmail.com>
2023-12-29 05:16:05 +0000

    requestUpdate

--- F2FChat/src/dashboardComponent/notificationDashboard/requestMessages.jsx
+++ F2FChat/src/dashboardComponent/notificationDashboard/requestMessages.jsx
@@ -3,10 +3,32 @@ export default function RequestMessages({ newRequest }) {
   return (
     <div>
       {newRequest.map((receiveRequest, index) => {
-        let { messengerPic, messenger, to, content, timeSent } = receiveRequest;
+        let { _id, messengerPic, messenger, to, content, timeSent } =
+          receiveRequest;
         return (
           <div key={index} className="request-container">
-            
+            <small className="message-id">#{_id}</small>
+            <h6 className="messenger-name">{messenger}</h6>
+            <h7
+             className={`${
+                index % 2 === 0 ? "receive-status1" : index % 2 === 1 ? "receive-status2" : "receive-status3"
+              }`}
+              
+            >
+              New
+            </h7>
+            <img
+              src={messengerPic}
+              alt="no-image"
+              className="messenger-picture"
+            />
+            <h6 className="request-message">{content}</h6>
+            <small className="request-time">{timeSent}</small>
+            <div className="request-buttons">
+              <button className="accept-btn">Accept</button>
+              <button className="decline-btn">Decline</button>
+            </div>
+            <button className="view-message">View</button>
           </div>
         );
       })}
