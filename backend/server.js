cac537901ac9475f25557c1b283ee0122833934a
Nicholas-Ayim <nicholasayim7@gmail.com>
2024-01-12 15:12:20 +0000

    updatedServer

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,8 @@ const { Server} = require("socket.io")
 const io = new Server(server,{
     cors:{
         origin:"http://localhost:5173",
-        methods:["GET","POST"]
+        methods:["GET","POST"],
+        credentials:true
     }
 })
 
