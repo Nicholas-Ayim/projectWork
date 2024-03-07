62a4ebf040de674f5e2c7a85c1cf19fb197c19f0
Nicholas Ayim Yaw Yirenkyi (NAYY) <99901352+Nicholas-Ayim@users.noreply.github.com>
2024-03-07 12:20:32 +0000

    Update chatApi.jsx

--- F2FChat/src/services/chatApi.jsx
+++ F2FChat/src/services/chatApi.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const chatApi = createApi({
   reducerPath: "chatApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/contact"
+    baseUrl: "https://project-work-api.vercel.app/contact"
   }),
   endpoints: (builder) => ({
     managerProfile: builder.mutation({
