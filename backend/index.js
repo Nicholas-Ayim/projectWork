fa5e5ea651e2341ca84d9d2592758b1495a98ac4
Nicholas Ayim Yaw Yirenkyi (NAYY) <99901352+Nicholas-Ayim@users.noreply.github.com>
2024-03-07 12:27:09 +0000

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(cors({
 ))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.get("/",(req,res)=>{
+app.get("/contact",(req,res)=>{
   res.json('deployed!!!')
 })
 const contactRoutes = require('./contactRoutes/contactRoutes')
